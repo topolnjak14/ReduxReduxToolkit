0c1d9704ea0695cf9b20a8b13a65149e5efd7f2e
unknown <josetopolnjak14@gmail.com>
2023-07-30 09:59:42 -0300

    Primeras queries en banner y comienzo del trabajo en aside

---
+++ src/app/aside/Aside.jsx
@@ -0,0 +1,31 @@
+"use client"
+import imagen from "../../../public/images/carrito.png";
+import Image from "next/image";
+import estilos from "./carrito.module.css"
+import React, {useState} from "react";
+import Carrito from "../carrito/Carrito";
+
+const Aside = () => {
+    const [mostrarAside, setMostrarAside] =useState(false);
+
+    const handleMostrarAside=()=>{
+        setMostrarAside(!mostrarAside);
+  
+    }
+    return (
+        <>
+            <div className={estilos.carrito} onClick={()=> handleMostrarAside()}>
+                <Image
+                    src={imagen}
+                    width={25}
+                    height={25}
+                />
+            </div>
+            {mostrarAside &&
+            (<Carrito/>)
+            }   
+        </>
+    );
+}
+
+export default Aside;
--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
-import imagen from "../../../public/images/carrito.png";
-import Image from "next/image";
 import estilos from "./nav.module.css";
+import Aside from "../aside/Aside";
 
 const Header = () => {
     return (
         <>
           <div className={estilos.nav}>
-                <Link href="/about" className={estilos.logo}>
+                <Link href="/" className={estilos.logo}>
                     TechVibe
                     <span>.</span>
                 </Link>
@@ -16,13 +15,7 @@ const Header = () => {
                 <Link href="/about">Hot sales</Link>
                 <Link href="/about">Pricing</Link>
             </div>
-            <div className={estilos.carrito}>
-                <Image
-                    src={imagen}
-                    width={25}
-                    height={25}
-                />
-            </div>
+                <Aside/>
           </div> 
         </>
     );
